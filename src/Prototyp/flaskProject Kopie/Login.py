17a596cae2a3650e0fb7deb62faa45c07374003f
Jens.Rosenkranz <s82099@htw-dresden.de>
2022-04-13 17:20:09 +0200

    Logindaten eingetragen

--- src/Prototyp/flaskProject Kopie/Login.py
+++ src/Prototyp/flaskProject Kopie/Login.py
@@ -3,12 +3,12 @@ import mysql.connector as mariadb
 
 # authorization for user trying to open application
 def LoginDB(user, pwd):
-    if mariadb.connect(user='test', password='test', host='rdbs.rz.htw-dresden.de', database='aaapartnerhs'):
+    if mariadb.connect(user='aaapartnerhs', password='a6D6d2c5X0', host='rdbs.rz.htw-dresden.de', database='aaapartnerhs'):
         return 'True'
     else:
         return 'False'
 
 
-# function to reconnect to db after usr is stored in session
+# function to reconnect to db after user is stored in session
 def newConnection():
-    return mariadb.connect(user='test', password='test', host='rdbs.rz.htw-dresden.de', database='aaapartnerhs')
+    return mariadb.connect(user='aaapartnerhs', password='a6D6d2c5X0', host='rdbs.rz.htw-dresden.de', database='aaapartnerhs')
