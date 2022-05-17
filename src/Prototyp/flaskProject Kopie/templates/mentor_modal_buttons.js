b23ebad27060dbae8cd52d2a40870df6bf8e6087
Pascal Thielemann <s82101@htw-dresden.de>
2022-05-17 10:45:37 +0200

    functionality added close button in mentor html

--- src/Prototyp/flaskProject Kopie/templates/mentor_modal_buttons.js
+++ src/Prototyp/flaskProject Kopie/templates/mentor_modal_buttons.js
@@ -1,6 +1,8 @@
 function addButtonFunctionality() {
     //set functionality for all abbrechen/X Buttons
     $(' .cancel').on('click', t => {
-       console.log(t);
+        //get modal of clicked button
+       let parent = t['currentTarget']['parentElement']['parentElement']['parentElement']['parentElement'];
+       parent.style.display = "none";
     });
 }
