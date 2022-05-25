222f03e376a81f71002a5ea55519106c4b4c1f33
Pascal Thielemann <s82101@htw-dresden.de>
2022-05-25 21:25:11 +0200

    filter for agreements implemented (90% done, still little bit buggy)

--- src/Prototyp/flaskProject Kopie/app.py
+++ src/Prototyp/flaskProject Kopie/app.py
@@ -20,8 +20,7 @@ def login_required(f):
         if 'usr' in session:
             return f(*args, **kwargs)
         else:
-
-        return redirect('/')
+            return redirect('/')
         #return f(*args, **kwargs)
     return wrap
 
