595a3cb8dbb865222c3047a74eba8247702dcfed
Jens.Rosenkranz <s82099@htw-dresden.de>
2022-06-24 16:58:29 +0200

    inster Mentor toggle wenn speichern #97

--- src/Prototyp/flaskProject Kopie/templates/mentor_modal_add.js
+++ src/Prototyp/flaskProject Kopie/templates/mentor_modal_add.js
@@ -11,6 +11,9 @@ function insertMentor() {
     })
         .done((data) => {
             console.log(data);
+            // wenn einfügen erfolgreich war, Modal wieder ausgeblendet
+            // sonst Fehlermeldung in Modal anzeigen
+           $('#modal_add_mentor').toggle();
         });
 
     });
